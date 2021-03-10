444c53a2a6f0e2e496576e250ead8df49e626e3d
jk <jk@vin.ovh>
2021-03-10 15:51:33 -0500

    don't patch glibc

--- pacman_setup.sh
+++ pacman_setup.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-patched_glibc=glibc-linux4-2.33-4-x86_64.pkg.tar.zst
-curl -LO https://repo.archlinuxcn.org/x86_64/$patched_glibc
-bsdtar -C / -xvf $patched_glibc
-rm -f $patched_glibc
-
 pacman -Syu --noconfirm git
+pacman -Scc
