b895744931abeeed50d6672ae5ac23a5443de0e4
jk <jk@vin.ovh>
2021-03-10 16:02:13 -0500

    what

--- pacman_setup.sh
+++ pacman_setup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+patched_glibc=glibc-linux4-2.33-4-x86_64.pkg.tar.zst
+curl -LO https://repo.archlinuxcn.org/x86_64/$patched_glibc
+bsdtar -C / -xvf $patched_glibc
+rm $patched_glibc
+
 pacman -Syu --noconfirm git
 pacman -Scc
